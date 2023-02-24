1585d1928bb2808a74b3836a75b4343e0a5d2363
Skwiera <mckskwiera@gmail.com>
2023-02-24 19:40:40 +0000

    manager class passes tests

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
-
 const render = require("./src/page-template.js");
-
 
 // TODO: Write Code to gather information about the development team members, and render the HTML file.
 
+let team = [];
+
+async function startProgram() {
+
+    //todo innquirer
+    team.push(new Manager("Magic", 32, "test@example.test"))
+
+    let htmlDoc = render(team)
+
+    await fs.writeFile(outputPath, htmlDoc)
+}
+
+
+
+startProgram();

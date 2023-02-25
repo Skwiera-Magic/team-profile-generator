3b90808b4ecaaabba66a4e5529b8dd33564b583f
Skwiera <mckskwiera@gmail.com>
2023-02-25 19:07:36 +0000

    engineer and intern are writing to file

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let team = [];
 async function startProgram() {
 
     //todo inquirer
-    var {name, id, email, officeNumber} = await inquirer
+    var { name, id, email, officeNumber } = await inquirer
         .prompt([
             {
                 type: 'input',
@@ -37,8 +37,108 @@ async function startProgram() {
                 name: 'officeNumber',
                 message: "What is the office number?"
             }
-        ]) 
+        ])
     team.push(new Manager(name, id, email, officeNumber))
+
+    var { name, id, email, gitHub } = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's id number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email address?"
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: "What is the engineer's GitHub username?"
+            }
+        ])
+    team.push(new Engineer(name, id, email, gitHub))
+
+    var { name, id, email, gitHub } = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the engineer's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the engineer's id number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the engineer's email address?"
+            },
+            {
+                type: 'input',
+                name: 'gitHub',
+                message: "What is the engineer's GitHub username?"
+            }
+        ])
+    team.push(new Engineer(name, id, email, gitHub))
+
+    var { name, id, email, school } = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's id number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email address?"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the intern's school name?"
+            }
+        ])
+    team.push(new Intern(name, id, email, school))
+
+    var { name, id, email, school } = await inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: "What is the intern's name?",
+            },
+            {
+                type: 'input',
+                name: 'id',
+                message: "What is the intern's id number?"
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: "What is the intern's email address?"
+            },
+            {
+                type: 'input',
+                name: 'school',
+                message: "What is the intern's school name?"
+            }
+        ])
+    team.push(new Intern(name, id, email, school))
 
     let htmlDoc = render(team)
 
